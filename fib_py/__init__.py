745fe077c1f43422b358656fb0a1860ea8842ea0
Him10Agr <himanshuagr.c2@gmail.com>
2025-03-05 00:59:21 +0530

    adding basic print function to check pip working status

---
+++ fib_py/__init__.py
@@ -0,0 +1,2 @@
+def say_hello() -> None:
+    print("The fib module is saying hello")
--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="fib.py",
     version="0.0.1",
     author="Himanshu Agrawal",
     author_email="himanshuagr.c2@gmail.com",
     description="Calculates the fibonacci number",
-    long_description="A basic library that \
-                    calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Him10Agr/fib",
     install_requires=[],
